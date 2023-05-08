83affa3620a5c89b3c232be87b0e46acff361223
Obinna Umerah <umerahobinna@gmail.com>
2023-05-08 16:12:59 -0500

    Added getters and setters for the Author class

--- src/main/java/com/books/project/model/Author.java
+++ src/main/java/com/books/project/model/Author.java
@@ -25,4 +25,36 @@ public class Author {
     @OneToMany(mappedBy = "author", orphanRemoval = true)
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<Author> authorList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Author> getAuthorList() {
+        return authorList;
+    }
+
+    public void setAuthorList(List<Author> authorList) {
+        this.authorList = authorList;
+    }
 }
