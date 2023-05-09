cc9dc5779ddbb46668d1a55217e0be0a7ca3f5fc
Obinna Umerah <umerahobinna@gmail.com>
2023-05-09 11:00:08 -0500

    Imported all the necessary dependencies

--- src/main/java/com/books/project/model/Book.java
+++ src/main/java/com/books/project/model/Book.java
@@ -2,88 +2,57 @@ package com.books.project.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
-
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
-@Table(name = "books")
+@Table(name="books")
 public class Book {
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column
     private String name;
-
     @Column
     private String description;
-
     @Column
     private String isbn;
 
-    @Column
-    private Long author_id;
+    @JsonIgnore
+    @ManyToOne //many books to one author
+    @JoinColumn(name = "author_id")
+    private Author author;
 
-    public Book(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
 
-    public Book(Long id, String name, String description, String isbn, Long author_id) {
+
+    public Book(Long id, String name, String description, String isbn) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.isbn = isbn;
-        this.author_id = author_id;
     }
 
-    public Book() {
+    public Book(){
 
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() {return id;}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setId(Long id) {this.id = id;}
 
-    public String getName() {
-        return name;
-    }
+    public String getName() {return name;}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) {this.name = name;}
 
-    public String getDescription() {
-        return description;
-    }
+    public String getDescription() {return description;}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public void setDescription(String description) {this.description = description;}
 
-    public String getIsbn() {
-        return isbn;
-    }
+    public String getIsbn() {return isbn;}
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
+    public void setIsbn(String isbn) {this.isbn = isbn;}
 
-    public Long getAuthor_id() {
-        return author_id;
-    }
 
-    public void setAuthor_id(Long author_id) {
-        this.author_id = author_id;
-    }
 
     @Override
     public String toString() {
@@ -92,14 +61,8 @@ public class Book {
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", author_id=" + author_id +
                 '}';
     }
-
-    @JsonIgnore
-    @ManyToOne
-    @JoinColumn(name = "author_id")
-    private Author author;
 
     public Author getAuthor() {
         return author;
@@ -108,6 +71,4 @@ public class Book {
     public void setAuthor(Author author) {
         this.author = author;
     }
-
-
 }
