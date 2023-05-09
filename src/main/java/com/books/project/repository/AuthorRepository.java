74e68ca321f2944a3cc7bef378321a513a40ef95
Obinna Umerah <umerahobinna@gmail.com>
2023-05-09 17:53:46 -0500

    Created findByName method

--- src/main/java/com/books/project/repository/AuthorRepository.java
+++ src/main/java/com/books/project/repository/AuthorRepository.java
@@ -2,8 +2,13 @@ package com.books.project.repository;
 
 import com.books.project.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.Id;
+
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Long>{
 
-
+    Author findByName(String name);
+    //Author findById();
 }
