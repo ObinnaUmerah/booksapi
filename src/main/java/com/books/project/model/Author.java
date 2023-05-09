7b3e26b01fb8e7c2a6fccbcdbf7d8c8aaf1e5d97
Obinna Umerah <umerahobinna@gmail.com>
2023-05-09 10:57:24 -0500

    Fixed mapping bug

--- src/main/java/com/books/project/model/Author.java
+++ src/main/java/com/books/project/model/Author.java
@@ -3,58 +3,62 @@ package com.books.project.model;
 
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
-
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table
+@Table(name = "authors")
 public class Author {
-
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column
-    private String firstName;
-
+    private String name;
     @Column
-    private String lastName;
+    private String description;
 
     @OneToMany(mappedBy = "author", orphanRemoval = true)
     @LazyCollection(LazyCollectionOption.FALSE)
-    private List<Author> authorList;
+    private List<Book> bookList;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
+    public Author(Long id, String name, String description) {
         this.id = id;
+        this.name = name;
+        this.description = description;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public Author() {
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public String getDescription() {return description;}
+
+    public void setDescription(String description) {this.description = description;}
+
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 
-    public String getLastName() {
-        return lastName;
+    public List<Book> getBookList() {
+        return bookList;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public List<Author> getAuthorList() {
-        return authorList;
-    }
-
-    public void setAuthorList(List<Author> authorList) {
-        this.authorList = authorList;
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
     }
 }
