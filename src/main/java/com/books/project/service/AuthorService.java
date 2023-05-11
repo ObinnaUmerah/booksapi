4ce3df9a2c4449355f674797c4778d63cefd87c5
Obinna Umerah <umerahobinna@gmail.com>
2023-05-11 12:11:27 -0500

    Added updateAuthor and deleteAuthor methods

--- src/main/java/com/books/project/service/AuthorService.java
+++ src/main/java/com/books/project/service/AuthorService.java
@@ -2,6 +2,8 @@ package com.books.project.service;
 
 import com.books.project.model.Author;
 import com.books.project.repository.AuthorRepository;
+import com.books.project.exception.InformationNotFoundException;
+import com.books.project.model.Author;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,7 +33,27 @@ public class AuthorService {
         return author;
     }
 
+    public Optional<Author> updateAuthor(Long authorId, Author authorObject) throws InformationNotFoundException {
+        Optional<Author> author = authorRepository.findById(authorId);
+        if (author.isPresent()) {
+            author.get().setName(authorObject.getName());
+            author.get().setDescription(authorObject.getDescription());
+            authorRepository.save(author.get());
+            return author;
+        } else {
+            throw new InformationNotFoundException("author with id " + authorId + " not found");
+        }
+    }
 
+    public Optional<Author> deleteAuthor(Long authorId) throws InformationNotFoundException {
+        Optional<Author> book = authorRepository.findById(authorId);
+        if (book.isPresent()) {
+            authorRepository.deleteById(authorId);
+            return book;
+        } else {
+            throw new InformationNotFoundException("book with id " + authorId + " not found");
+        }
+    }
 
 
 }
