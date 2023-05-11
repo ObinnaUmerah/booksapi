e4a5af7e5bcdd78d5edd10359403852ff09bfb5e
Obinna Umerah <umerahobinna@gmail.com>
2023-05-11 11:56:51 -0500

    All 4 CRUD endpoints are fully functioning.

--- src/main/java/com/books/project/repository/AuthorRepository.java
+++ src/main/java/com/books/project/repository/AuthorRepository.java
@@ -4,11 +4,11 @@ import com.books.project.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Id;
+import java.util.Optional;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long>{
 
     Author findByName(String name);
-    //Author findById();
+    Optional<Author> findById(Long id);
 }
