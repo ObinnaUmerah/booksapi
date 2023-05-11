e779863a88129bc83a4d374073036c71875c06fe
Obinna Umerah <umerahobinna@gmail.com>
2023-05-11 11:57:10 -0500

    All 4 CRUD endpoints are fully functioning.

--- src/main/java/com/books/project/controller/AuthorController.java
+++ src/main/java/com/books/project/controller/AuthorController.java
@@ -2,6 +2,7 @@ package com.books.project.controller;
 
 
 import com.books.project.exception.InformationExistException;
+import com.books.project.exception.InformationNotFoundException;
 import com.books.project.model.Author;
 import com.books.project.repository.AuthorRepository;
 import com.books.project.service.AuthorService;
@@ -18,15 +19,8 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class AuthorController {
 
-
     @Autowired
     private AuthorService authorService;
-
-//    @Autowired
-//    public void setAuthorRepository(AuthorRepository authorRepositoryRepository){
-//        this.authorRepository = authorRepository;
-//    }
-
     static HashMap<String, Object> message = new HashMap<>();
 
     @GetMapping(path = "/hello-world/")
@@ -34,36 +28,14 @@ public class AuthorController {
         return "hello World";
     }
 
-//    @GetMapping(path = "/books/")
-//    public ResponseEntity<?> getAllAuthors() {
-//        List<Author> bookList = authorRepository.getAllAuthors(); (bookList.isEmpty()) {
-//            message.put("message", "cannot find any books ");
-//            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
-//        } else {
-//            message.put("message", "success");
-//            message.put("data", bookList);
-//            return new ResponseEntity<>(message, HttpStatus.OK);
-//        }
 
-//    @PostMapping(path = "/authors/")
-//    public Author createAuthor(@RequestBody Author authorObject) {
-//        System.out.println("service calling createAuthor ==>");
-//
-//        Author category = authorRepository.findByName(authorObject.getName());
-//        if (category != null) {
-//            throw new InformationExistException("category with name " + category.getName() + " already exists");
-//        } else {
-//            return authorRepository.save(authorObject);
-//        }
-//    }
-
-    @PostMapping("/author/")
+    @PostMapping("/authors/")
     public ResponseEntity<?> createAuthor(@RequestBody Author author) {
         Author newAuthor = authorService.createAuthor(author);
         if (newAuthor != null) {
-           message.put("message", "success");
+            message.put("message", "success");
             message.put("data", newAuthor);
-           return new ResponseEntity<>(message, HttpStatus.CREATED);
+            return new ResponseEntity<>(message, HttpStatus.CREATED);
         } else {
             message.put("message", "unable to create a book at this time");
             return new ResponseEntity<>(message, HttpStatus.OK);
@@ -72,26 +44,50 @@ public class AuthorController {
 
     }
 
-    @GetMapping("/author/{authorId}")
-    public ResponseEntity<?> getAuthor(@PathVariable(value = "authorId") Long authorId){
+    @GetMapping("/authors/{authorId}")
+    public ResponseEntity<?> getAuthor(@PathVariable(value = "authorId") Long authorId) {
         Optional<Author> author = authorService.getAuthorById(authorId);
-//        if(author.isPresent()){
-//            message.put("message", "success");
-////            message.put("data", author.get());
-//            return new ResponseEntity<>(message, HttpStatus.OK);
-
-
-
-//            message.put("message", "cannot find book with id " + bookId);
+        if (author.isPresent()) {
+            message.put("message", "success");
+            message.put("data", author.get());
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        } else {
+            message.put("message", "cannot find book with id " + authorId);
             return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
         }
-
-
     }
 
 
+    // http://localhost:8080
+    @PutMapping("/authors/{authorId}")
+    public ResponseEntity<?> updateAuthor(@PathVariable(value = "authorId") Long authorId, @RequestBody Author authorObject) throws InformationNotFoundException {
+        Optional<Author> bookToUpdate = authorService.updateAuthor(authorId, authorObject);
+        if (bookToUpdate.isEmpty()) {
+            message.put("message", "cannot find book with id " + authorId);
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        } else {
+            message.put("message", "book with id " + authorId + " has been successfully updated");
+            message.put("data", bookToUpdate.get());
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+
+    }
+
+    @DeleteMapping("/authors/{authorId}")
+    public ResponseEntity<?> deleteAuthor(@PathVariable(value = "authorId") Long authorId) {
+        Optional<Author> authorToDelete = authorService.deleteAuthor(authorId);
+        if (authorToDelete.isEmpty()) {
+            message.put("message", "cannot find book with id " + authorId);
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        } else {
+            message.put("message", "book with id " + authorId + " has been successfully deleted");
+            message.put("data", authorToDelete.get());
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+    }
 
 
+}
 
-    //@GetMapping(path ="/")
+
 
