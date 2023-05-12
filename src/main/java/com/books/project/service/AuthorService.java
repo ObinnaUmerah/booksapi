90248b8c8c6405a20e2b857eb15d1f01ce048e34
Obinna Umerah <umerahobinna@gmail.com>
2023-05-12 14:09:18 -0500

    Fixed bug by adding the setBookRepository method and autowiring it. Also create createAuthorBook method.

--- src/main/java/com/books/project/service/AuthorService.java
+++ src/main/java/com/books/project/service/AuthorService.java
@@ -1,24 +1,34 @@
 package com.books.project.service;
 
 import com.books.project.model.Author;
+import com.books.project.model.Book;
 import com.books.project.repository.AuthorRepository;
 import com.books.project.exception.InformationNotFoundException;
 import com.books.project.model.Author;
+import com.books.project.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 public class AuthorService {
 
     private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
 
     @Autowired
     public void setAuthorRepository(AuthorRepository authorRepository){
         this.authorRepository = authorRepository;
     }
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
 
     public List<Author> getAllAuthors(){
         return authorRepository.findAll();
@@ -52,6 +62,18 @@ public class AuthorService {
             return book;
         } else {
             throw new InformationNotFoundException("book with id " + authorId + " not found");
+        }
+    }
+
+    public Book createAuthorBook(Long authorId, Book bookObject) {
+        System.out.println("service calling createCategoryRecipe ==>");
+        try {
+            Optional author = authorRepository.findById(authorId);
+            bookObject.setAuthor((Author) author.get());
+            bookRepository.save(bookObject);
+            return bookObject;
+        } catch (NoSuchElementException e) {
+            throw new InformationNotFoundException("author with id " + authorId + " not found");
         }
     }
 
