534df3327fba46e09e52da6bf627c99fd27c318d
Obinna Umerah <umerahobinna@gmail.com>
2023-05-15 02:27:17 -0500

    Added docstrings

--- src/main/java/com/books/project/model/Author.java
+++ src/main/java/com/books/project/model/Author.java
@@ -14,54 +14,115 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
-    private String name;
+    private String firstname;
     @Column
-    private String description;
+    private String lastname;
+
 
     @OneToMany(mappedBy = "author", orphanRemoval = true)
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<Book> bookList;
 
-    public Author(String name, String description) {
-        this.name = name;
-        this.description = description;
+    /**
+     * A constructor that generates an author in the authors table.
+     * @param firstname The first name of the author.
+     * @param lastname The last name of the author.
+     */
+
+    public Author(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
     }
 
-    public Author(Long id, String name, String description) {
+    /**
+     * A constructor that generates an author in the authors table.
+     * @param id The id where the author is stored in the table.
+     * @param firstname The first name of the author.
+     * @param lastname The last name of the author.
+     */
+    public Author(Long id, String firstname, String lastname) {
         this.id = id;
-        this.name = name;
-        this.description = description;
+        this.firstname = firstname;
+        this.lastname = lastname;
     }
 
+    /**
+     * A constructor that generates an author in the authors table
+     */
     public Author() {
     }
 
+    /**
+     * Gets the id of an author
+     * @return The id of the author specified by the user.
+     */
     public Long getId() {return id;}
 
+    /**
+     * Sets the id of an author
+     * @param id The id that the user wants to change the current author to.
+     */
     public void setId(Long id) {this.id = id;}
 
-    public String getName() {return name;}
 
-    public void setName(String name) {this.name = name;}
+    /**
+     * Retrieves the first name of the author
+     * @return The first name of the author
+     */
+    public String getFirstname() {
+        return firstname;
+    }
 
-    public String getDescription() {return description;}
+    /**
+     * Changes the first name of the author
+     * @param firstname The first name of the author
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-    public void setDescription(String description) {this.description = description;}
+    /**
+     * Retrieves the last name of the author
+     * @return The last name of the author
+     */
+    public String getLastname() {
+        return lastname;
+    }
 
+    /**
+     * Changes the last name of the author
+     * @param lastname The last name of the author
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
+    /**
+     * Displays the information about an author
+     * @return The information of the author.
+     */
     @Override
     public String toString() {
         return "Author{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", bookList=" + bookList +
                 '}';
     }
 
+    /**
+     * Retrieves the author's book collection.
+     * @return The author's list of books.
+     */
     public List<Book> getBookList() {
         return bookList;
     }
 
+    /**
+     * Changes the author's book collection.
+     * @param bookList The author's list of books.
+     */
     public void setBookList(List<Book> bookList) {
         this.bookList = bookList;
     }
